1c04f92ea99a563cad044b7b2e5f868a810ee7aa
BW <bmwanz@live.com>
2018-12-13 11:01:44 -0800

    create project

---
+++ Clipboard2WebSearch/app/src/main/java/com/maximumscrubbage/bw/clipboard2websearch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.maximumscrubbage.bw.clipboard2websearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
